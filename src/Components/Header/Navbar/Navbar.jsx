1f1452a895d8ff733f7c44b6e0d9346cdbecf7bc
nazmulhossen1 <nazmulhossen539@gmail.com>
2023-10-09 19:57:39 +0600

    change some style

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-sm lg:hidden mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul className="  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-700 w-52">
+                    <ul className="  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#858b9f] w-32 rounded-md">
                         {navLink}
                     </ul>
                 </div>
